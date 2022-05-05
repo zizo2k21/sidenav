85290ea9a0195f34ab410ad8cb1c404b7aafcc9d
zizo2k21 <yedjaziz@yahoo.fr>
2022-05-05 21:50:41 +0200

    sidenav

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,37 @@
+export const navbarData =[
+    {
+        routelink : 'dashboard',
+        icon : 'fal fa-home',
+        label : 'Dashboard'
+    },
+    {
+        routelink : 'products',
+        icon : 'fal fa-box-open',
+        label : 'Products'
+    },
+    {
+        routelink : 'statistics',
+        icon : 'fal fa-chart-bar',
+        label : 'Statistics'
+    },
+    {
+        routelink : 'coupens',
+        icon : 'fal fa-tags',
+        label : 'Coupens'
+    },
+    {
+        routelink : 'pages',
+        icon : 'fal fa-file',
+        label : 'Pages'
+    },
+    {
+        routelink : 'media',
+        icon : 'fal fa-camera',
+        label : 'Media'
+    },
+    {
+        routelink : 'settings',
+        icon : 'fal fa-cog',
+        label : 'Settings'
+    },
+]
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Output,EventEmitter, HostListener } from '@angular/core';
+
+import { navbarData } from './nav-data';
+
+
+interface SideNavToggle{
+  screenWidth: number;
+  collapsed: boolean;
+  }
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+  collapsed = false;
+  screenWidth =0;
+  navData = navbarData;
+
+  @HostListener ('window:resize', ['$event'])
+  onResize(event:any){
+    this.screenWidth = window.innerWidth; 
+    if(this.screenWidth<=768){
+      this.collapsed =false;
+      this.onToggleSideNav.emit({collapsed:this.collapsed, screenWidth: this.screenWidth});
+
+    }
+  }
+   
+
+  toggleCollapse(): void{
+
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed:this.collapsed, screenWidth: this.screenWidth});
+
+  }
+
+  closeSidenav(): void{
+
+    this.collapsed = false; 
+    this.onToggleSideNav.emit({collapsed:this.collapsed, screenWidth: this.screenWidth});
+
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth
+  }
+
+}
